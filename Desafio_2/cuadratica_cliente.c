fc7faed080f140caa3202c2a5063e1106f0b4f7a
Francisco Valle <cescovalledev@gmail.com>
2022-10-07 08:57:53 -0600

    Desafio practico 2 Terminado

--- Desafio_2/cuadratica_cliente.c
+++ Desafio_2/cuadratica_cliente.c
@@ -4,22 +4,26 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <complex.h>
 
 int main()
 {
-    int fd, n;
-    int a, b, c;
+    int fd,fdflag;
+    int a, b, c, flag = 0;
 
     int buf[3];
-    float total[2];
+    complex double total[2];
     // Dirección del archivo FIFO
     char *mififo = "/tmp/mififo";
+    char *flagfifo= "/tmp/flagfifo";
 
     // Crear archivo FIFO
     mkfifo(mififo, 0666);
+    mkfifo(flagfifo,0666);
 
     printf("*** Bienvenido a la calculadora de Cuadraticas ***\n");
     fd = open(mififo, O_WRONLY);
+    fdflag=open(flagfifo, O_WRONLY);
 
     // Entrada de Datos
     printf("*** Digite los datos de entrada ***");
@@ -37,16 +41,31 @@ int main()
     // Escribe el input en la FIFO y lo cierra
     write(fd, buf, sizeof(buf));
     close(fd);
+    write(fdflag, &flag, sizeof(flag));
+    close(fdflag);
 
     // Abrir FIFO en solo lectura
     fd = open(mififo, O_RDONLY);
+    fdflag=open(flagfifo, O_RDONLY);
     // Leer FIFO
     read(fd, total, sizeof(total));
+    read(fdflag, &flag, sizeof(flag));
 
     // Leer el mensaje
-    printf("Los resultados son: %f y %f", total[0], total[1]);
+    printf("flag: %d\n", flag);
+    if (flag == 0)
+    {
+        printf("Los resultados son: \nx1:%f \nx2:%f", creal(total[0]), creal(total[1]));
+    }
+    else
+    {
+        printf("Los resultado complejos son: \nx1: %.2lf+%.2lfi",creal(total[0]),cimag(total[0]));
+        printf("\nx2: %.2lf+%.2lfi",creal(total[1]),cimag(total[1]));
+    }
+
     printf("\n\n");
     close(fd);
+    close(fdflag);
 
     return 0;
 }
--- Desafio_2/cuadratica_servidor.c
+++ Desafio_2/cuadratica_servidor.c
@@ -4,31 +4,48 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <math.h>
+#include <complex.h>
 
 int main()
 {
-    int fd1, n;
-    int a, b, c;
+    int fd1,fdflag1, n;
+    int a, b, c, flag;
 
     int buf[3];
-    float total[2];
+    complex double total[2], x1, x2;
+    float d;
     // Dirección del archivo FIFO
     char *mififo = "/tmp/mififo";
+    char *flagfifo= "/tmp/flagfifo";
     fd1 = open(mififo, O_RDONLY);
+    fdflag1=open(flagfifo, O_RDONLY);
     read(fd1, buf, sizeof(buf));
-
+    read(fdflag1, &flag, sizeof(flag));
     a = buf[0];
     b = buf[1];
     c = buf[2];
-    //printf("a: %d, b: %d, c: %d", a, b, c);
-    //Proceso de cuadratica
-    total[0] = b + c;
-    total[1] = c - a;
+    // printf("a: %d, b: %d, c: %d", a, b, c);
+    // Proceso de cuadratica
+    d = (b * b - (4 * a * c));
+
+    x1 = ((-b + csqrt(d)) / (2 * a));
+    x2 = ((-b - csqrt(d)) / (2 * a));
+    total[0] = x1;
+    total[1] = x2;
     close(fd1);
+    close(fdflag1);
 
     // Abriendo la FIFO para escribir el resultado
+    if (d < 0)
+    {
+        flag = 1;
+    }
     fd1 = open(mififo, O_WRONLY);
+    fdflag1=open(flagfifo, O_WRONLY);
+    write(fdflag1, &flag, sizeof(flag));
     write(fd1, total, sizeof(total));
     close(fd1);
+    close(fdflag1);
     return 0;
 }
