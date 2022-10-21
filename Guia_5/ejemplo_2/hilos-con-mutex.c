6b0b414574185695ec9451cb758c5cf818bb61c7
Francisco Valle <cescovalledev@gmail.com>
2022-10-21 08:15:01 -0600

    ejemplo 2 mutex

--- Guia_5/ejemplo_2/hilos-con-mutex.c
+++ Guia_5/ejemplo_2/hilos-con-mutex.c
@@ -3,27 +3,33 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+pthread_mutex_t varmutex;
+
 void *funcion01(void *arg)
 {
     int i;
+    pthread_mutex_lock(&varmutex);
     for (i = 0; i < 100; i++)
     {
         printf("*");
         fflush(stdout);
         sleep(1);
     }
+    pthread_mutex_unlock(&varmutex);
     return NULL;
 }
 
 void *funcion02(void *arg)
 {
     int i;
+    pthread_mutex_lock(&varmutex);
     for (i = 0; i < 100; i++)
     {
         printf("o");
         fflush(stdout);
         sleep(1);
     }
+    pthread_mutex_unlock(&varmutex);
     return NULL;
 }
 
@@ -31,7 +37,7 @@ int main(int argc, char const *argv[])
 {
     int i;
     pthread_t h1, h2;
-
+    pthread_mutex_init(&varmutex, NULL);
     if (pthread_create(&h1, NULL, funcion01, NULL))
     {
         printf("error creando hilo 1");
@@ -44,6 +50,7 @@ int main(int argc, char const *argv[])
     }
     pthread_join(h1, NULL);
     pthread_join(h2, NULL);
+    pthread_mutex_destroy(&varmutex);
     printf("\n");
     return 0;
 }
